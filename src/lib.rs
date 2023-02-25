8cd517e7d1f9d23e0a6d0bd59a5be2203d88f413
ichdenkenicht <dix@goerlichs.de>
2023-02-25 21:23:29 +0100

    pga + regs+ commands + bits

--- src/lib.rs
+++ src/lib.rs
@@ -1,261 +1,250 @@
-
-
-
 #[deny(missing_docs)]
 extern crate embedded_hal as hal;
 
 use hal::spi::blocking::{SpiBus, SpiBusRead, SpiBusWrite, SpiDevice};
 
+mod commands;
+use commands::Command;
 
-pub enum Command {
-    RESET = 0b0000_0110,    //Resets the config registers to all zero
-    START = 0b0000_1000,    //Starts a single conversion or the continous conversions
-    POWERDOWN = 0b0000_0010,//Power down
-    RDATA = 0b0001_0000,    //Loads the output shift register with the most recent result
-    RREG = 0b0010_0000,     //0010_rrnn / rr -> address / nn -> number of bytes - 1
-    WREG = 0b0100_0000,     //0100_rrnn / rr -> address / nn -> number of bytes - 1
-
-}
-
-impl Command {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub enum Register {
-    CONFIG0 = 0x00,
-    CONFIG1 = 0x01,
-    CONFIG2 = 0x02,
-    CONFIG3 = 0x03,
-}
-
-impl Register {
-    fn addr(self) -> u8 {
-        self as u8
-    }
-}
-
-///Programmable Gain Amplifier (pga) ads1220 datasheet, p. 40
-#[derive(Debug, Copy, Clone, Default)]
-pub enum PGA {
-    #[default]
-    Gain1 = 0b000,
-    Gain2 = 0b001,
-    Gain4 = 0b010,
-    Gain8 = 0b011,
-    Gain16 = 0b100,
-    Gain32 = 0b101,
-    Gain64 = 0b110,
-    Gain128 = 0b111,
-}
-
-impl PGA {
-    pub fn bits(self) -> u8 {
-        self as u8
-    }
-
-    pub fn val(self) -> u8 {
-        1 << self as u8
-    }
-}
-
-//Channel
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Channel {
-    #[default]
-    DIFF_AIN0_AIN1 = 0x0,
-    DIFF_AIN0_AIN2 = 0x1,
-    DIFF_AIN0_AIN3 = 0x2,
-    DIFF_AIN1_AIN2 = 0x3,
-    DIFF_AIN1_AIN3 = 0x4,
-    DIFF_AIN2_AIN3 = 0x5,
-    DIFF_AIN1_AIN0 = 0x6,
-    DIFF_AIN3_AIN2 = 0x7,
-    AIN0 = 0x8,
-    AIN1 = 0x9,
-    AIN2 = 0xA,
-    AIN3 = 0xB,
-    VREFDIFF4 = 0xC,
-    AVDIFF4 = 0xD,
-    VCC2 = 0xE,
-    RESERVED = 0xF,
-}
-
-impl Channel {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-//Data Rate Normal Mode
-#[derive(Debug, Copy, Clone, Default)]
-pub enum DataRateNormal {
-    #[default]
-    SPS20 = 0x0,
-    SPS45 = 0x1,
-    SPS90 = 0x2,
-    SPS175 = 0x3,
-    SPS330 = 0x4,
-    SPS600 = 0x5,
-    SPS1000 = 0x6
-}
-
-impl DataRateNormal {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Mode {
-    #[default]
-    NORMAL = 0x0,
-    DUTYCYCLE = 0x1,
-    TURBO = 0x2,
-}
-
-impl Mode {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Reference {
-    #[default]
-    V2048 = 0x0,
-    REF0 = 0x1,
-    REF1 = 0x2,
-    AV = 0x3,
-}
-
-impl Reference {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Filter {
-    #[default]
-    NONE = 0x0,
-    HZ5060 = 0x1,
-    HZ50 = 0x2,
-    HZ60 = 0x3,
-}
-
-impl Filter {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Idac {
-    #[default]
-    OFF = 0x0,
-    U10 = 0x1,
-    U50 = 0x2,
-    U100 = 0x3,
-    U250 = 0x4,
-    U500 = 0x5,
-    U1000 = 0x6,
-    U1500 = 0x7,
-}
-
-impl Idac {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Idac1r {
-    #[default]
-    OFF = 0x0,
-    AIN0 = 0x1,
-    AIN1 = 0x2,
-    AIN2 = 0x3,
-    AIN3 = 0x4,
-    REFP0 = 0x5,
-    REFN0 = 0x6,
-}
-
-impl Idac1r {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Idac2r {
-    #[default]
-    OFF = 0x0,
-    AIN0 = 0x1,
-    AIN1 = 0x2,
-    AIN2 = 0x3,
-    AIN3 = 0x4,
-    REFP0 = 0x5,
-    REFN0 = 0x6,
-}
-
-impl Idac2r {
-    fn bits(self) -> u8 {
-        self as u8
-    }
-}
-
+mod registers;
+use registers::{*, BitFlags as BF};
 
 #[derive(Debug, Copy, Clone, Default)]
 pub struct Config {
-    cfg0: u8,
-    cfg1: u8,
-    cfg2: u8,
-    cfg3: u8,
+    pub bits: u8,
 }
 
-
+impl Config {
+    pub fn with_high(&self, mask: u8) -> Self {
+        Config {
+            bits: self.bits | mask,
+        }
+    }
+    pub fn with_low(&self, mask: u8) -> Self {
+        Config {
+            bits: self.bits & !mask,
+        }
+    }
+}
 
 pub struct ADS1220<SPI> {
     spi: SPI,
-    config: Config,
+    config0: Config,
+    config1: Config,
+    config2: Config,
+    config3: Config,
 }
 
 impl<SPI> ADS1220<SPI>
 where
-     SPI: SpiDevice,
-     SPI::Bus: SpiBus, // or SpiBusRead/SpiBusWrite if you only need to read or only write.
+    SPI: SpiDevice,
+    SPI::Bus: SpiBus, // or SpiBusRead/SpiBusWrite if you only need to read or only write.
 {
-     pub fn new(spi: SPI) -> Self {
-         Self { spi, config: Config::default() }
-     }
-
-    pub fn read_foo(&mut self) -> Result<[u8; 2], MyError<SPI::Error>> {
-        let mut buf = [0; 2];
-
-         // `transaction` asserts and deasserts CS for us. No need to do it manually!
-        self.spi.transaction(|bus| {
-             bus.write(&[0x90])?;
-             bus.read(&mut buf)
-        }).map_err(MyError::Spi)?;
-
-        Ok(buf)
+    pub fn new(spi: SPI) -> Self {
+        Self {
+            spi,
+            config0: Config::default(),
+            config1: Config::default(),
+            config2: Config::default(),
+            config3: Config::default(),
+        }
     }
 
-    pub fn read_register(&mut self, reg: Register) -> Result<u8, MyError<SPI::Error>> {
+    pub fn reset(&mut self) -> Result<(), MyError<SPI::Error>> {
+        self.spi
+            .transaction(|bus| bus.write(&[Command::RESET.bits()]))
+            .map_err(MyError::Spi)?;
+        Ok(())
+    }
+
+    pub fn start(&mut self) -> Result<(), MyError<SPI::Error>> {
+        self.spi
+            .transaction(|bus| bus.write(&[Command::START.bits()]))
+            .map_err(MyError::Spi)?;
+        Ok(())
+    }
+
+    pub fn powerdown(&mut self) -> Result<(), MyError<SPI::Error>> {
+        self.spi
+            .transaction(|bus| bus.write(&[Command::POWERDOWN.bits()]))
+            .map_err(MyError::Spi)?;
+        Ok(())
+    }
+
+    fn read_register(&mut self, reg: Register) -> Result<u8, MyError<SPI::Error>> {
         let mut res: [u8; 1] = [0];
 
         // `transaction` asserts and deasserts CS for us. No need to do it manually!
-        self.spi.transaction(|bus| {
-             bus.write(&[Command::RREG.bits() | reg.addr() << 2])?;
-             bus.read(&mut res)
-        }).map_err(MyError::Spi)?;
+        self.spi
+            .transaction(|bus| {
+                bus.write(&[Command::RREG.bits() | reg.addr() << 2])?;
+                bus.read(&mut res)
+            })
+            .map_err(MyError::Spi)?;
 
         Ok(res[0])
     }
 
+    fn write_register(&mut self, reg: Register, value: u8) -> Result<(), MyError<SPI::Error>> {
+        // `transaction` asserts and deasserts CS for us. No need to do it manually!
+        self.spi
+            .transaction(|bus| {
+                bus.write(&[Command::WREG.bits() | reg.addr() << 2])?;
+                bus.write(&[value])
+            })
+            .map_err(MyError::Spi)?;
+
+        Ok(())
+    }
+
+    fn read(&mut self) -> Result<u32, MyError<SPI::Error>> {
+        let mut res = [0u8; 4];
+        self.spi
+            .transaction(|bus| {
+                bus.write(&[Command::RDATA.bits()])?;
+                bus.read(&mut res[1..])
+            })
+            .map_err(MyError::Spi)?;
+        Ok(u32::from_be_bytes(res))
+    }
+
+    ///Set Programmable Gain Bypass
+    pub fn set_pga_bypass(&mut self, bp: PGA_BYPASS) -> Result<(), MyError<SPI::Error>> {
+        let config = match bp {
+            PGA_BYPASS::ENABLED => {
+                self.config0.with_low(BF::PB)
+            },
+            PGA_BYPASS::DISABLED => {
+                self.config0.with_high(BF::PB)
+            },
+        };
+        self.write_register(Register::CONFIG0, config.bits)?;
+        self.config0 = config;
+        Ok(())
+    }
+    
+    ///Set DataReady
+    pub fn set_drdym(&mut self, drdym: DRDYM) -> Result<(), MyError<SPI::Error>> {
+        let config = match drdym {
+            DRDYM::DRDY => {
+                self.config3.with_low(BF::DRDYM)
+            },
+            DRDYM::DOUT_DRDY => {
+                self.config3.with_high(BF::DRDYM)
+            },
+        };
+        self.write_register(Register::CONFIG3, config.bits)?;
+        self.config3 = config;
+        Ok(())
+    }
+    
+    ///Set Low-side Power Switch
+    pub fn set_psw(&mut self, psw: PSW) -> Result<(), MyError<SPI::Error>> {
+        let config = match psw {
+            PSW::OPEN => {
+                self.config2.with_low(BF::PSW)
+            },
+            PSW::CLOSING => {
+                self.config2.with_high(BF::PSW)
+            },
+        };
+        self.write_register(Register::CONFIG2, config.bits)?;
+        self.config2 = config;
+        Ok(())
+    }
+    
+    ///Set Conversion Mode
+    pub fn set_mode(&mut self, mode: CM) -> Result<(), MyError<SPI::Error>> {
+        let config = match mode {
+            CM::SINGLE => {
+                self.config1.with_low(BF::CM)
+            },
+            CM::CONTINUOUS => {
+                self.config1.with_high(BF::CM)
+            },
+        };
+        self.write_register(Register::CONFIG1, config.bits)?;
+        self.config1 = config;
+        Ok(())
+    }
+    
+    ///Set Temperature sensor mode
+    pub fn set_temp_mode(&mut self, mode: TS) -> Result<(), MyError<SPI::Error>> {
+        let config = match mode {
+            TS::DISABLED => {
+                self.config1.with_low(BF::TS)
+            },
+            TS::ENABLED => {
+                self.config1.with_high(BF::TS)
+            },
+        };
+        self.write_register(Register::CONFIG1, config.bits)?;
+        self.config1 = config;
+        Ok(())
+    }
+    
+    pub fn set_gain(&mut self, gain: PGA) -> Result<(), MyError<SPI::Error>> {
+        
+        let config = match gain {
+            PGA::Gain1 => {
+                self.config0.with_low(0x03 << 1)
+            },
+            PGA::Gain2 => {
+                self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain2.bits_on_pos())
+            },
+            PGA::Gain4 => {
+                self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain4.bits_on_pos())
+            },
+            PGA::Gain8 => {
+                self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain8.bits_on_pos())
+            },
+            PGA::Gain16 => {
+                self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain16.bits_on_pos())
+            },
+            PGA::Gain32 => {
+                self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain32.bits_on_pos())
+            },
+            PGA::Gain64 => {
+                self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain64.bits_on_pos())
+            },
+            PGA::Gain128 => {
+                //self.config0.with_low(0x03 << 1);
+                self.config0.with_high(PGA::Gain128.bits_on_pos())
+            },
+        };
+        self.write_register(Register::CONFIG0, config.bits)?;
+        self.config0 = config;
+        
+        Ok(())
+    }
+    
+    pub fn set_channel(&mut self, ch: Channel) -> Result<(), MyError<SPI::Error>> {
+        
+        let config = match ch {
+            Channel::DIFF_AIN0_AIN1 => {
+                
+            },
+            Channel::DIFF_AIN0_AIN2 => {
+                
+            },
+            Channel::DIFF_AIN0_AIN3 => {
+                
+            },
+            
+        };
+        
+        
+        self.write_register(Register::CONFIG0, config.bits)?;
+        self.config0 = config;
+        
+        Ok(())
+    }
     
 }
 
---
+++ src/registers/mod.rs
@@ -0,0 +1,298 @@
+//! Register abstractions.
+//!
+//! Register abstractions for the 4 config registers of the ADS1220 ADC from TI
+//!
+//! https://www.ti.com/lit/ds/symlink/ads1220.pdf
+
+
+///Register 0-3 Bitflags
+pub struct BitFlags;
+#[allow(unused)]
+impl BitFlags {
+    //Reg 0
+    ///PGA_BYPASS bitflag
+    pub const PB: u8 = 0b0000_0001; 
+        
+    //Reg 1
+    ///Burn-Out current source bitflag
+    pub const BCS: u8 = 0b0000_0001;  
+    
+    ///Temperatur Sensor Mode bitflag
+    pub const TS: u8 = 0b0000_0010; 
+       
+    ///Conversion Mode Single or Continous bitflag
+    pub const CM: u8 = 0b0000_0100;     
+    //Reg 2
+    ///Low side switch bitflag
+    pub const PSW: u8 = 0b0000_1000;    
+    //Reg 3
+    ///Data ready mode bitflag
+    pub const DRDYM: u8 = 0b0000_0010;  
+}
+
+
+#[derive(Debug, Copy, Clone)]
+pub enum Register {
+    CONFIG0 = 0x00,
+    CONFIG1 = 0x01,
+    CONFIG2 = 0x02,
+    CONFIG3 = 0x03,
+}
+
+impl Register {
+    pub fn addr(self) -> u8 {
+        self as u8
+    }
+}
+
+
+//Reg0
+
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum PGA_BYPASS {
+    #[default]
+    ENABLED = 0,
+    DISABLED = 1,
+}
+
+///Programmable Gain Amplifier (pga) ads1220 datasheet, p. 40
+#[derive(Debug, Copy, Clone, Default)]
+pub enum PGA {
+    #[default]
+    Gain1 = 0b0000_0000,
+    Gain2 = 0b0000_0001,
+    Gain4 = 0b0000_0010,
+    Gain8 = 0b0000_0011,
+    Gain16 = 0b0000_0100,
+    Gain32 = 0b0000_0101,
+    Gain64 = 0b0000_0110,
+    Gain128 = 0b0000_0111,
+}
+
+impl PGA {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 1
+    }
+}
+
+//Channel
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Channel {
+    #[default]
+    DIFF_AIN0_AIN1 = 0x0,
+    DIFF_AIN0_AIN2 = 0x1,
+    DIFF_AIN0_AIN3 = 0x2,
+    DIFF_AIN1_AIN2 = 0x3,
+    DIFF_AIN1_AIN3 = 0x4,
+    DIFF_AIN2_AIN3 = 0x5,
+    DIFF_AIN1_AIN0 = 0x6,
+    DIFF_AIN3_AIN2 = 0x7,
+    AIN0 = 0x8,
+    AIN1 = 0x9,
+    AIN2 = 0xA,
+    AIN3 = 0xB,
+    VREFDIFF4 = 0xC,
+    AVDIFF4 = 0xD,
+    VCC2 = 0xE,
+    RESERVED = 0xF,
+}
+
+impl Channel {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 4
+    }
+}
+
+//Reg 1
+
+///Burn-our current source
+#[derive(Debug, Copy, Clone, Default)]
+pub enum BCS {
+    #[default]
+    OFF = 0,
+    ON = 1,
+}
+
+///Temperatur sensor mode
+#[derive(Debug, Copy, Clone, Default)]
+pub enum TS {
+    #[default]
+    DISABLED = 0,
+    ENABLED = 1,
+}
+
+///Conversion Mode
+#[derive(Debug, Copy, Clone, Default)]
+pub enum CM {
+    #[default]
+    SINGLE = 0,
+    CONTINUOUS = 1,
+}
+
+///Operation Mode
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Mode {
+    #[default]
+    NORMAL = 0x0,
+    DUTYCYCLE = 0x1,
+    TURBO = 0x2,
+}
+
+impl Mode {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 3
+    }
+}
+
+///Data Rate Normal Mode
+#[derive(Debug, Copy, Clone, Default)]
+pub enum DataRateNormal {
+    #[default]
+    SPS20 = 0x0,
+    SPS45 = 0x1,
+    SPS90 = 0x2,
+    SPS175 = 0x3,
+    SPS330 = 0x4,
+    SPS600 = 0x5,
+    SPS1000 = 0x6
+}
+
+impl DataRateNormal {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 5
+    }
+}
+
+//Reg 2
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Idac {
+    #[default]
+    OFF = 0x0,
+    U10 = 0x1,
+    U50 = 0x2,
+    U100 = 0x3,
+    U250 = 0x4,
+    U500 = 0x5,
+    U1000 = 0x6,
+    U1500 = 0x7,
+}
+
+impl Idac {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+}
+
+///Low-side power switch config
+#[derive(Debug, Copy, Clone, Default)]
+pub enum PSW {
+    #[default]
+    OPEN = 0,
+    CLOSING = 1,
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Filter {
+    #[default]
+    NONE = 0x0,
+    HZ5060 = 0x1,
+    HZ50 = 0x2,
+    HZ60 = 0x3,
+}
+
+impl Filter {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 4
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Reference {
+    #[default]
+    V2048 = 0x0,
+    REF0 = 0x1,
+    REF1 = 0x2,
+    AV = 0x3,
+}
+
+impl Reference {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 6
+    }
+}
+
+
+
+//Reg 3
+
+
+///Data Ready Mode
+#[derive(Debug, Copy, Clone, Default)]
+pub enum DRDYM {
+    #[default]
+    DRDY = 0,
+    DOUT_DRDY = 1,
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Idac1r {
+    #[default]
+    OFF = 0x0,
+    AIN0 = 0x1,
+    AIN1 = 0x2,
+    AIN2 = 0x3,
+    AIN3 = 0x4,
+    REFP0 = 0x5,
+    REFN0 = 0x6,
+}
+
+impl Idac1r {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 2
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Idac2r {
+    #[default]
+    OFF = 0x0,
+    AIN0 = 0x1,
+    AIN1 = 0x2,
+    AIN2 = 0x3,
+    AIN3 = 0x4,
+    REFP0 = 0x5,
+    REFN0 = 0x6,
+}
+
+impl Idac2r {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+    pub fn bits_on_pos(self) -> u8 {
+        (self as u8) << 5
+    }
+}
